ae54e90b3b40e514884913d8089d4ff29efd986e
amandamigliorini <amanda.migliorini@hotmail.com>
2025-01-18 10:06:22 -0300

    Week 02 done with comments

--- week02/code/TakingTurnsQueue_Tests.cs
+++ week02/code/TakingTurnsQueue_Tests.cs
@@ -11,7 +11,10 @@ public class TakingTurnsQueueTests
     // Scenario: Create a queue with the following people and turns: Bob (2), Tim (5), Sue (3) and
     // run until the queue is empty
     // Expected Result: Bob, Tim, Sue, Bob, Tim, Sue, Tim, Sue, Tim, Tim
-    // Defect(s) Found: 
+    // Defect(s) Found: Expected:<Bob>. Actual:<Sue>.
+    //                  Method Enqueue on PersonQueue.cs: _queue.Insert(0, person); Corrected to _queue.Add(person);
+    //                  Method GetNextPerson on TakingTurnsQueue.cs had problems on the logics, 
+    //                  conditions needed to be corrected;               
     public void TestTakingTurnsQueue_FiniteRepetition()
     {
         var bob = new Person("Bob", 2);
@@ -43,7 +46,10 @@ public class TakingTurnsQueueTests
     // Scenario: Create a queue with the following people and turns: Bob (2), Tim (5), Sue (3)
     // After running 5 times, add George with 3 turns.  Run until the queue is empty.
     // Expected Result: Bob, Tim, Sue, Bob, Tim, Sue, Tim, George, Sue, Tim, George, Tim, George
-    // Defect(s) Found: 
+    // Defect(s) Found: Expected:<Bob>. Actual:<Sue>.
+    //                  Method Enqueue on PersonQueue.cs: _queue.Insert(0, person); Corrected to _queue.Add(person);
+    //                  Method GetNextPerson on TakingTurnsQueue.cs had problems on the logics, 
+    //                  conditions needed to be corrected;
     public void TestTakingTurnsQueue_AddPlayerMidway()
     {
         var bob = new Person("Bob", 2);
@@ -85,7 +91,11 @@ public class TakingTurnsQueueTests
     // Scenario: Create a queue with the following people and turns: Bob (2), Tim (Forever), Sue (3)
     // Run 10 times.
     // Expected Result: Bob, Tim, Sue, Bob, Tim, Sue, Tim, Sue, Tim, Tim
-    // Defect(s) Found: 
+    // Defect(s) Found: Expected:<Bob>. Actual:<Sue>.
+    //                  Method Enqueue on PersonQueue.cs: _queue.Insert(0, person); Corrected to _queue.Add(person);
+    //                  Method GetNextPerson on TakingTurnsQueue.cs had problems on the logics, 
+    //                  conditions needed to be corrected. The infinite turns wasn't there.
+
     public void TestTakingTurnsQueue_ForeverZero()
     {
         var timTurns = 0;
@@ -116,7 +126,10 @@ public class TakingTurnsQueueTests
     // Scenario: Create a queue with the following people and turns: Tim (Forever), Sue (3)
     // Run 10 times.
     // Expected Result: Tim, Sue, Tim, Sue, Tim, Sue, Tim, Tim, Tim, Tim
-    // Defect(s) Found: 
+    // Defect(s) Found: Expected:<Tim>. Actual:<Sue>.
+    //                  Method Enqueue on PersonQueue.cs: _queue.Insert(0, person); Corrected to _queue.Add(person);
+    //                  Method GetNextPerson on TakingTurnsQueue.cs had problems on the logics, 
+    //                  conditions needed to be corrected. The infinite turns wasn't there.             
     public void TestTakingTurnsQueue_ForeverNegative()
     {
         var timTurns = -3;
@@ -143,7 +156,7 @@ public class TakingTurnsQueueTests
     [TestMethod]
     // Scenario: Try to get the next person from an empty queue
     // Expected Result: Exception should be thrown with appropriate error message.
-    // Defect(s) Found: 
+    // Defect(s) Found: None - The test passed.
     public void TestTakingTurnsQueue_Empty()
     {
         var players = new TakingTurnsQueue();
